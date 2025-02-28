8f59cee419696ac92540e6916eeaa4e350f53ab1
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-28 15:32:00 +0530

    added axios function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-async function getRecentPost(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    const data = await response.json();
-    console.log(data);
-    document.getElementById("post").innerHTML=data.body;
-}
+// async function getRecentPost(){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//     const data = await response.json();
+//     console.log(data);
+//     document.getElementById("post").innerHTML=data.body;
+// }
 
-getRecentPost();
+getRecent();
+
+
+// Another method using axios function
+// By using axios function the data which is ffetched from the API is automatically converted to a json file.
+
+async function getRecent(){
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1")
+    document.getElementById("post").innerHTML= res.data.title;
+    console.log("request processed")
+}
