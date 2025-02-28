ab6ce0024bb3ef50533bb6b7dee7fce3d8066f87
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-28 18:02:53 +0530

    added some middlewares

--- index.js
+++ index.js
@@ -2,49 +2,67 @@ const express = require("express");
 const app = express();
 const port = 3002;
 
-app.get("/sum/:a/:b", (req, res) => {
+let requestCount = 0;
+function requestIncreaser(req, res, next){
+    requestCount = requestCount + 1;
+    console.log(`Total number of requests handled by the server are ${requestCount}`);
+    next();
+}
+// logs the method, timestamp and the url
+
+function middleware(req, res, next){
+    console.log(`The given request was handled using ${req.method} method.`);
+    console.log(`The given request was accessed on the url ${req.hostname}`);
+    console.log(new Date())
+    next();
+}
+
+app.use(middleware);
+app.use(requestIncreaser);
+
+app.get("/sum/:a/:b",  (req, res) => {
     const a = parseFloat(req.params.a);
     const b = parseFloat(req.params.b);
-    console.log(a+b);
+    
     res.json(
         {
             ans: a+b
         }
     )
-    // res.send(`The sum is ${a+b}`);
+     
 });
 app.get("/multiply", (req, res) => {
     const a = parseFloat(req.query.a);
     const b = parseFloat(req.query.b);
-    console.log(a*b);
+    
     res.json(
         {
             ans: a*b
         }
     )
-    // res.send(`The sum is ${a+b}`);
+    
 });
 app.get("/diff/:a/:b", (req, res) => {
     const a = parseFloat(req.params.a);
     const b = parseFloat(req.params.b);
-    console.log(a-b);
+    
     res.json(
         {
             ans: a-b
         }
     )
-    // res.send(`The sum is ${a+b}`);
+    
 });
 app.get("/div/:a/:b", (req, res) => {
     const a = parseFloat(req.params.a);
     const b = parseFloat(req.params.b);
-    console.log(a/b);
+    
     res.json(
         {
             ans: a/b
         }
     )
-    // res.send(`The sum is ${a+b}`);
+    
 });
 
 
