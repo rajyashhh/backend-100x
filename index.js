c43bda6c214deb47e7c884280e6a219a0fa8e450
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-28 16:12:46 +0530

    backend calculator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.get("/sum/:a/:b", (req, res) => {
     const a = parseFloat(req.params.a);
@@ -9,6 +9,39 @@ app.get("/sum/:a/:b", (req, res) => {
     res.json(
         {
             ans: a+b
+        }
+    )
+    // res.send(`The sum is ${a+b}`);
+});
+app.get("/multiply", (req, res) => {
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
+    console.log(a*b);
+    res.json(
+        {
+            ans: a*b
+        }
+    )
+    // res.send(`The sum is ${a+b}`);
+});
+app.get("/diff/:a/:b", (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    console.log(a-b);
+    res.json(
+        {
+            ans: a-b
+        }
+    )
+    // res.send(`The sum is ${a+b}`);
+});
+app.get("/div/:a/:b", (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    console.log(a/b);
+    res.json(
+        {
+            ans: a/b
         }
     )
     // res.send(`The sum is ${a+b}`);
